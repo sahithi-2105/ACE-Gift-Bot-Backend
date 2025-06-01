5a765fdb795cfb2623d923aa941a477216558c58
sahithi-2105 <sahithisiddireddy05@gmail.com>
2025-06-01 22:20:43 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ACE-GiftBot.vercel.app"],
+    origin: ["http://localhost:5173", "https://ace-giftbot.vercel.app"],
     credentials: true,
   })
 );
